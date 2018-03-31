7c7a335d3d175b61b058b333cf88450a401cad83
limeiling <li.meiling01@whaley.cn>
2018-03-31 12:17:17 +0800

    'hi'

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(function(registration) {
+	navigator.serviceWorker.register('./sw.js').then(function(registration) {
 		// 注册成功
 		console.log('ServiceWorker registration successful with scope: ', registration.scope);
 	}).catch(function(err) {
